65c305b2f6bea8e6fdf75e2b05766fb87986f178
Raț Adrian <adirat10@gmail.com>
2022-04-24 18:25:03 +0300

    add invelitoarea convexa

---
+++ Invelitoarea_convexa_algoritm_slab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invelitoarea_convexa_algoritm_slab
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
