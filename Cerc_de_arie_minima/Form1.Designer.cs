a8f81b298833f3e916f9d89ae0859348a4af526a
Raț Adrian <adirat10@gmail.com>
2022-04-24 12:24:52 +0300

    add cerc

---
+++ Cerc_de_arie_minima/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Cerc_de_arie_minima
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(14, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(781, 436);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(682, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(615, 391);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Aria: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Cerc_de_arie_minima/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cerc_de_arie_minima
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black, 2);
+            Pen p2 = new Pen(Color.LightSkyBlue, 3);
+
+            Random r = new Random();
+            int n = r.Next(2, 50);
+
+
+            PointF[] m1 = new PointF[n];
+            float diametru = float.MinValue;
+            float xa = 0, ya = 0, xb = 0, yb = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                m1[i].X = r.Next(100, panel1.Width - 150);
+                m1[i].Y = r.Next(100, panel1.Height - 150);
+                g.DrawEllipse(p, m1[i].X, m1[i].Y, 1.5F, 1.5F);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (distanta(m1[i].X, m1[i].Y, m1[j].X, m1[j].Y) > diametru)
+                    {
+                        diametru = distanta(m1[i].X, m1[i].Y, m1[j].X, m1[j].Y);
+                        xa = m1[i].X;
+                        ya = m1[i].Y;
+                        xb = m1[j].X;
+                        yb = m1[j].Y;
+                    }
+                }
+            }
+            float xcentru, ycentru, raza;
+            xcentru = (xa + xb) / 2;
+            ycentru = (ya + yb) / 2;
+            //raza = distanta(xcentru, ycentru, xa, ya);
+            raza = diametru / 2;
+
+            g.DrawEllipse(p2, xcentru - raza, ycentru - raza, diametru, diametru);
+            //g.DrawLine(p2, xa, ya, xb, yb);
+            double Aria = Math.PI * raza * raza;
+            label1.Text = Convert.ToString(Aria);
+        }
+        private float distanta(float x1, float y1, float x2, float y2)
+        {
+            float dist = (float)(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            return dist;
+        }
+    }
+}
