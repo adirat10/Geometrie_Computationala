454457464cff7ebd733aa0e8bfaa93d2ada107a2
Raț Adrian <adirat10@gmail.com>
2022-05-17 08:36:03 +0300

    add desenare poligon

---
+++ Partitionarea_in_poligoane_monotone/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Partitionarea_in_poligoane_monotone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(483, 396);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 42);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Done";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 396);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 42);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Partitionarea_in_poligoane_monotone/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Partitionarea_in_poligoane_monotone
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen pen = new Pen(Color.RoyalBlue, 3);
+        const int raza = 3;
+        int n = 0; // nr de varfuri ale poligonului
+        List<PointF> p = new List<PointF>(); //lista varfurilor
+        bool ok = true;
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            p.Add(this.PointToClient(new Point(Form1.MousePosition.X, Form1.MousePosition.Y)));
+            if (ok)
+            {
+                g.DrawEllipse(pen, p[n].X, p[n].Y, raza, raza);
+                g.DrawString(Convert.ToString(letters[n]), new Font(FontFamily.GenericSansSerif, 14),
+                new SolidBrush(Color.Black), p[n].X + raza, p[n].Y - raza);
+                if (n > 0)
+                    g.DrawLine(pen, p[n - 1], p[n]);
+                n++;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (n < 3)
+                return;
+            g.DrawLine(pen, p[n - 1], p[0]);
+            ok = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
